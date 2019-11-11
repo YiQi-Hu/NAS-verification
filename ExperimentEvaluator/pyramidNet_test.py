9f8eaa10142eacb6e2071c90ab71a33c07cc0d61
jalynn <jalynnlau@outlook.com>
2019-11-11 18:39:44 +0800

    pickle encoding

--- ExperimentEvaluator/pyramidNet_test.py
+++ ExperimentEvaluator/pyramidNet_test.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import keras
-from keras.datasets import cifar10
+from keras.datasets.cifar import load_batch
+from keras import backend as K
 import sys
 from keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
@@ -273,6 +274,39 @@ class PyramidNet:
         )
 
 
+def load_data():
+    """Loads CIFAR10 dataset.
+
+    # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    """
+    dirname = 'cifar-10-batches-py'
+    origin = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+    path = os.path.join('/data/data',dirname)
+
+    num_train_samples = 50000
+
+    x_train = np.empty((num_train_samples, 3, 32, 32), dtype='uint8')
+    y_train = np.empty((num_train_samples,), dtype='uint8')
+
+    for i in range(1, 6):
+        fpath = os.path.join(path, 'data_batch_' + str(i))
+        (x_train[(i - 1) * 10000: i * 10000, :, :, :],
+         y_train[(i - 1) * 10000: i * 10000]) = load_batch(fpath)
+
+    fpath = os.path.join(path, 'test_batch')
+    x_test, y_test = load_batch(fpath)
+
+    y_train = np.reshape(y_train, (len(y_train), 1))
+    y_test = np.reshape(y_test, (len(y_test), 1))
+
+    if K.image_data_format() == 'channels_last':
+        x_train = x_train.transpose(0, 2, 3, 1)
+        x_test = x_test.transpose(0, 2, 3, 1)
+
+    return (x_train, y_train), (x_test, y_test)
+
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 device_name = tf.test.gpu_device_name()
@@ -303,7 +337,7 @@ epochs = 300
 weight_decay = 1e-4
 lr = 0.1
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+(x_train, y_train), (x_test, y_test) = load_data()
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 train_gen = ImageDataGenerator(
